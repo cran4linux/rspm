911174d69f1ac51afb1b107d789c8e459e0d770d
Iñaki Úcar <iucar@fedoraproject.org>
2022-07-28 10:38:33 +0200

    add package manual page

---
+++ R/rspm.R
@@ -0,0 +1,18 @@
+#' \pkg{rspm}: RStudio Package Manager
+#'
+#' Enables binary package installations on Linux distributions.
+#' Provides access to RStudio's public repos, and transparent management of
+#' system requirements without administrative privileges. Currently supported
+#' distributions are CentOS / RHEL 7 and 8, and several RHEL derivatives
+#' (Rocky Linux 8, AlmaLinux 8, Oracle Linux 7 and 8, Amazon Linux 2),
+#' openSUSE / SLES 15.3, and Ubuntu 18.04, 20.04 and 22.04.
+#'
+#' @author Iñaki Ucar
+#'
+#' @references \url{https://enchufa2.github.io/rspm}
+#'
+#' @docType package
+#' @name rspm-package
+#'
+#' @seealso \code{\link{manager}}, \code{\link{integration}}
+NULL
