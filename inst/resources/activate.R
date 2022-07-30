a1d2a92a264132dcadcc4c59e76e9b4185951e60
Iñaki Úcar <iucar@fedoraproject.org>
2022-07-30 17:18:08 +0200

    improve installation on activation

--- inst/resources/activate.R
+++ inst/resources/activate.R
@@ -1,9 +1,12 @@
 local({
-  if (!requireNamespace("rspm", quietly=TRUE))
-    tryCatch({
-      renv::install("rspm@VERSION")
-    }, error = function(e) {
-      renv::install("Enchufa2/rspm@VERSION")
-    })
+  try_install_if_not_available <- function(pkg, source) {
+    if (!requireNamespace(pkg, quietly=TRUE))
+      try(renv::install(source), silent=TRUE)
+  }
+
+  try_install_if_not_available("rspm@VERSION")
+  try_install_if_not_available("Enchufa2/rspm@VERSION")
+  try_install_if_not_available("Enchufa2/rspm")
+
   try(rspm::enable(), silent=TRUE)
 })
