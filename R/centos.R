9e11d476f3c7582d4caee7cefdc74eb4a67da2d3
Philipp Baumann <baumann-philipp@proton.me>
2022-11-19 15:49:48 +0100

    try fix "change root directory not permitted"

--- R/centos.R
+++ R/centos.R
@@ -5,18 +5,28 @@ centos_install <- function(pkgs) {
   old <- setwd(temp)
   on.exit(setwd(old))
   system(centos_cmd(), p(pkgs))
-  system("rpm -i --nodeps --noscripts --notriggers --nosignature --excludedocs",
-         "-r", user_dir(), "*")
+  if (system("id -u") != 0) {
+    rpm_list <- list.files(pattern = ".rpm")
+    for (rpm in rpm_list) {
+      system("rpm2cpio", rpm, "| cpio", "--directory", user_dir(), "-id")
+    }
+  } else {
+    system(
+      "rpm -i --nodeps --noscripts --notriggers --nosignature --excludedocs",
+      "-r", user_dir(), "*"
+    )
+  }
 }
 
 centos_install_sysreqs <- function(libs) {
   cat("Downloading and installing sysreqs...\n")
-  centos_install(paste0("*/", libs, collapse=" "))
+  centos_install(paste0("*/", libs, collapse = " "))
 }
 
 centos_cmd <- function() {
   cmd <- "yumdownloader --resolve"
-  if (Sys.which("dnf") != "")
+  if (Sys.which("dnf") != "") {
     cmd <- "dnf download --resolve"
+  }
   cmd
 }
