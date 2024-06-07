61cf67cee040c6e8b8d745a4569be8dcacd4dd70
Iñaki Ucar <iucar@fedoraproject.org>
2024-06-07 11:32:21 +0200

    enable Pop_OS (#26)

--- R/zzz_debian_like.R
+++ R/zzz_debian_like.R
@@ -1,8 +1,11 @@
 debian_requirements ->
-  ubuntu_requirements
+  ubuntu_requirements ->
+  pop_requirements
 
 debian_install ->
-  ubuntu_install
+  ubuntu_install ->
+  pop_install
 
 debian_install_sysreqs ->
-  ubuntu_install_sysreqs
+  ubuntu_install_sysreqs ->
+  pop_install_sysreqs
