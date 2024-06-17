c6fda050b5660da6f2fb4dbd8da809018871b1af
Iñaki Úcar <iucar@fedoraproject.org>
2024-06-17 19:49:29 +0200

    enable AL2023

--- R/rspm.R
+++ R/rspm.R
@@ -5,15 +5,14 @@
 #' \url{https://packagemanager.posit.co}, and transparent management of
 #' system requirements without administrative privileges. Currently supported
 #' distributions are CentOS / RHEL 7-9, and several RHEL derivatives
-#' (Rocky Linux, AlmaLinux, Oracle Linux, Amazon Linux 2),
+#' (Rocky Linux, AlmaLinux, Oracle Linux, Amazon Linux),
 #' openSUSE / SLES 15.4-5, Debian 11-12, and Ubuntu LTS 20-22.
 #'
 #' @author Iñaki Ucar
 #'
 #' @references \url{https://cran4linux.github.io/rspm/}
 #'
-#' @docType package
 #' @name rspm-package
 #'
 #' @seealso \code{\link{manager}}, \code{\link{integration}}
-NULL
+"_PACKAGE"
--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 - **Fully user mode**, no root privileges required.
 - Support for **CentOS / RHEL** 7, 8 and 9.
 - Support for **RHEL derivatives**:
-  Rocky Linux 8-9, AlmaLinux 8-9, Oracle Linux 7-9, Amazon Linux 2.
+  Rocky Linux 8-9, AlmaLinux 8-9, Oracle Linux 7-9, Amazon Linux 2-2023.
 - Support for **openSUSE / SLES** 15.4 and 15.5.
 - Support for **Debian** 11 and 12 (requires `apt-file`).
 - Support for **Ubuntu** 20.04 and 22.04 (requires `apt-file`).
