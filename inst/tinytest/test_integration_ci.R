dfcddf696685aeb5873ee13dd6e3275a240eafa1
Iñaki Úcar <iucar@fedoraproject.org>
2024-06-20 10:10:15 +0200

    move all integration tests to CI file to avoid CRAN issues

--- inst/tinytest/test_integration_ci.R
+++ inst/tinytest/test_integration_ci.R
@@ -1,10 +1,32 @@
 if (!at_home())
   exit_file("not in a CI environment")
 
-supported <- c("debian", "ubuntu", "centos", "rocky", "almalinux",
+supported <- c("debian", "ubuntu", "pop", "centos", "rocky", "almalinux",
                "ol", "rhel", "amzn", "sles", "opensuse")
 
 expect_silent(os <- rspm:::os())
 expect_inherits(os, "list")
 expect_true(os$id %in% supported)
-exit_file(paste("code:", os$code))
+message(paste("code:", os$code))
+
+# user agent
+expect_true(grepl(getRversion(), getOption("HTTPUserAgent")))
+
+# user dir
+expect_true(dir.exists(rspm:::user_dir()))
+
+# tracing and repo setting
+untrace(utils::install.packages)
+expect_false(inherits(utils::install.packages, "functionWithTrace"))
+
+enable()
+expect_true(inherits(utils::install.packages, "functionWithTrace"))
+expect_true("RSPM" %in% names(getOption("repos")))
+expect_true(grepl(rspm:::os()$code, getOption("repos")["RSPM"]))
+
+tracer <- paste(body(utils::install.packages), collapse="")
+expect_true(grepl("rspm::install_sysreqs()", tracer, fixed=TRUE))
+
+disable()
+expect_false(inherits(utils::install.packages, "functionWithTrace"))
+expect_false("RSPM" %in% names(getOption("repos")))
